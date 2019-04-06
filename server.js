f25166188b8075043abbe12d6536a5435df8cd30
kimjoshjp <kimjoshjp@gmail.com>
2019-04-06 16:16:17 +1000

    change port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello Tokyo from Puppet Pipelines. Your Node.js application is working!\n');
-}).listen(80);
+}).listen(3000);
 
 console.log('Server started');
