bd988b104b0f1ad53336cd363d1924d8ca1ce32d
kimjoshjp <kimjoshjp@gmail.com>
2019-04-07 16:56:46 +1000

    change  to BNE

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Tokyo from Puppet Pipelines. Your Node.js application is working!\n');
+    response.end('Hello Brisbane from Puppet Pipelines. Your Node.js application is working!\n');
 }).listen(3000);
 
 console.log('Server started');
